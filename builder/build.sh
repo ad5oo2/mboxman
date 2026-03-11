6cfe454cc192cc7dfd96f1217f91f3ee40a9bd43
Denis Arbuzov <denis.arbuzov@gmail.com>
2026-03-11 16:30:07 +0700

    slightly moving to k8s

--- builder/build.sh
+++ builder/build.sh
@@ -8,3 +8,11 @@ npm install
 npm run build
 mkdir -p /data/frontend
 cp -R /root/src/client/dist/* /data/frontend
+
+
+mkdir -p /data/backend
+cp -R /root/src/server/* /data/backend
+cd /data/backend
+npm install
+npx prisma generate
+
--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-cp /usr/src/app/crontab /etc/crontab
-rm /etc/cron.d/*
-rm /etc/cron.daily/*
-service cron start
-node /usr/src/app/src/server.js
+crond
+/usr/local/bin/node /data/backend/src/server.js
