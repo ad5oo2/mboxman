29c44b4d20638d9b64f587b3dce57ac68c58e620
Denis Arbuzov <denis.arbuzov@gmail.com>
2026-03-20 17:28:47 +0700

    prisma(db) issues with domain_fk

--- server/prisma/migrations/20221110112326_default_values_for_lastlogin_dovecot_plugin/migration.sql
+++ server/prisma/migrations/20221110112326_default_values_for_lastlogin_dovecot_plugin/migration.sql
@@ -1,4 +1,4 @@
 -- AlterTable
-ALTER TABLE `virtual_users` MODIFY `domain_id` INTEGER NOT NULL DEFAULT 0,
+ALTER TABLE `virtual_users` MODIFY `domain_id` INTEGER NULL,
     MODIFY `name` VARCHAR(100) NOT NULL DEFAULT '',
     MODIFY `password` VARCHAR(106) NOT NULL DEFAULT '';
